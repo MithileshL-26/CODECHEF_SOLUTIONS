cc2f6ec888664e3e91158d59d183a2b5f547e994
Mithilesh Lathkar <119594987+MithileshL-26@users.noreply.github.com>
2022-12-01 20:59:34 +0530

    Create KNGTOR | Tour of King.cpp

---
+++ STARTERS 67/KNGTOR | Tour of King.cpp
@@ -0,0 +1,39 @@
+/*
+* PROBLEM LINK : https://www.codechef.com/problems-old/KNGTOR
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define PI 3.141592653589793238462
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+
+void solve()
+{
+	int tc;
+	cin >> tc;
+	
+	while(tc--)
+	{
+		int N, M;
+		cin >> N >> M;
+		
+		cout << (N * 5) + (M * 7) << nline;
+	}
+}
+int main()
+{	
+	fastio();
+	
+	solve();
+	
+	return 0;
+}
